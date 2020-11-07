19c8c9aa7ac97ab858ca89083fbf194826ec12fa
shunya matsubara <shun.fzs1000@gmail.com>
2020-11-07 19:29:05 +0900

    expressのディレクトリを初期作成するシェルスクリプトを作成した

---
+++ web/bash/init-express/make-express.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+set -e
+
+initialize() {
+  yarn
+}
+
+mkcdir ()
+{
+  # mkdir -p: ディレクトリがなければ作成する
+  # cd -P: シンボリックリンクによる論理的なパスではなく実際のパスをたどる
+  # 引数を "" で囲っておかないと、仮に$1の中に"hoge fuga"といったスペースが含まれていたら、複数の引数としてパースされてしまう
+  # -- をつけておかないと、例えば$1が "-a" だった時に、ディレクトリ名ではなくコマンドオプションとして渡されてしまう
+  mkdir -p -- "$1" && cd -P -- "$1"
+}
+
+CURRENT_DIR=$PWD
+DIR=$1
+
+if [ $DIR ]
+then
+  echo "initializing project at $DIR"
+  echo "current directory: $CURRENT_DIR"
+
+  mkcdir $DIR
+  cp "$CURRENT_DIR/index.js" "index.js"
+  cp "$CURRENT_DIR/package.json" "package.json"
+  initialize
+else
+  echo "please specify target directory"
+fi
