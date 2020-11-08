394aefb2ea9d7a37baf5156d0552a47ef4c44cd1
shunya matsubara <shun.fzs1000@gmail.com>
2020-11-08 15:42:30 +0900

    サードパーティのyoutube

---
+++ web/thirdparty-cookie-demo/index.js
@@ -0,0 +1,50 @@
+const cookieParser = require('cookie-parser');
+const express = require('express');
+
+const app = express();
+
+const port = 8080;
+
+const mockDB = {};
+
+app.use(cookieParser());
+
+app.use((req, res, next) => {
+  const referer = req.header('referer');
+
+  if (mockDB[referer]) {
+    mockDB[referer] += 1;
+  } else {
+    mockDB[referer] = 1;
+  }
+
+  console.log(JSON.stringify(mockDB));
+
+  next();
+}, express.static('public-adsense', {
+  setHeaders: (res, path, stat) => {
+    res.cookie('id', 1, {
+      sameSite: 'none',
+      secure: true,
+    });
+  },
+}));
+
+app.get('/', (req, res) => {
+  res.send('Hello world!');
+});
+app.listen(port, () => {
+  console.log(`server started at http://localhost:${port}`);
+});
+
+const siteA = express();
+siteA.use(express.static('public-a'));
+siteA.listen(8081, () => {
+  console.log('おいらは8081で待機してるよ');
+});
+
+const siteB = express();
+siteB.use(express.static('public-b'));
+siteB.listen(8082, () => {
+  console.log('おいらは8082で待機してるよ');
+});
