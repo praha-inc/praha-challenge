7fa4683ae1af8334f7ccf71f33f8937ddd8f64d6
shunya matsubara <shun.fzs1000@gmail.com>
2020-11-18 08:03:40 +0900

    init exchangeType

---
+++ architecture/exchangeTypeWithState/src/index.ts
@@ -0,0 +1,20 @@
+import express, { Request, Response } from "express";
+import "reflect-metadata";
+
+const app = express();
+const port = 8080; // default port to listen
+
+app.use(express.json());
+
+// define a route handler for the default home page
+app.get("/", (_: Request, res: Response) => {
+  res.send("Hello world!");
+});
+app.post("/", (req: Request, res: Response) => {
+  res.send(`body2: ${JSON.stringify(req.body)}`);
+});
+
+// start the Express server
+app.listen(port, () => {
+  console.log(`server started at http://localhost:${port}`);
+});
