979e189090987d73e51b30105a5094c1faf34b2e
shunya matsubara <shun.fzs1000@gmail.com>
2020-11-18 09:34:42 +0900

    add tests

--- architecture/exchangeTypeWithState/src/employee.ts
+++ architecture/exchangeTypeWithState/src/employee.ts
@@ -1,11 +1,10 @@
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
 /* eslint-disable @typescript-eslint/no-parameter-properties */
 
-class Employee {
+export class Employee {
   public static readonly ENGINEER: number = 0;
   public static readonly SALESMAN: number = 1;
   public static readonly MANAGER: number = 2;
-  public static readonly SUPER_MANAGER: number = 3;
   public constructor(
     private _type: number,
     private _monthlySalary: number,
@@ -22,21 +21,31 @@ class Employee {
         return this._monthlySalary + this._comission;
       case Employee.MANAGER:
         return this._monthlySalary + this._bonus;
-      case Employee.SUPER_MANAGER:
-        return this._monthlySalary + this._comission + this._bonus;
       default:
         throw new Error("invalid employee type");
     }
   }
-  public retirementPlan() {
+  // memo: 退職プランを適用するか否か
+  public hasRetirementPlan() {
+    switch (this._type) {
+      case Employee.ENGINEER:
+        return true;
+      case Employee.SALESMAN:
+        return false;
+      case Employee.MANAGER:
+        return true;
+      default:
+        throw new Error("invalid employee type");
+    }
+  }
+  // memo: 取締役か否か
+  public isBoardMember() {
     switch (this._type) {
       case Employee.ENGINEER:
         return false;
       case Employee.SALESMAN:
         return false;
       case Employee.MANAGER:
-        return true;
-      case Employee.SUPER_MANAGER:
         return true;
       default:
         throw new Error("invalid employee type");
