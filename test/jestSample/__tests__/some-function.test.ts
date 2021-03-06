190e24b805194d9b227e7bc7cfdff4f9b8c44966
shunya matsubara <shun.fzs1000@gmail.com>
2021-03-06 20:55:04 +0900

    :recycle: storybookを初期化しただけのstorybook-introはもういらない

--- test/jestSample/__tests__/some-function.test.ts
+++ test/jestSample/__tests__/some-function.test.ts
@@ -1,19 +1,10 @@
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
 import { randomObject } from "../util/some-function";
 
-// jest.mock("../util/some-function");
+process.argv = ["hoge", "fuga"];
 
-describe("some-function", () => {
-  it("returns random!", () => {
-    expect(randomObject.randomFunction()).toBe("random!");
-  });
-  it("spy", () => {
-    jest.spyOn(randomObject, "randomFunction");
-    expect(randomObject.randomFunction()).toBe("random!");
-  });
-  it("mock", () => {
-    const mockFn = jest.fn().mockImplementationOnce(() => "not random!");
-    randomObject.randomFunction = mockFn;
-    expect(randomObject.randomFunction()).toBe("not random!");
-    expect(randomObject.randomFunction).toHaveBeenCalledTimes(1);
+describe("argv", () => {
+  it("argvを取れる", () => {
+    expect(process.argv).toEqual(["hoge", "fuga"]);
   });
 });
